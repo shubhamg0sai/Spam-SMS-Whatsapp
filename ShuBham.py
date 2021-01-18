7ed7f25626d834970cd5b692c0e3cf7fde27b4f2
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-01-18 08:15:21 +0530

    Update ShuBham.py

--- ShuBham.py
+++ ShuBham.py
@@ -1,22 +1,3 @@
-import os
-CorrectUsername = "g0sain"
-CorrectPassword = "Cl0ning"
-
-loop = 'true'
-while (loop == 'true'):
-    username = raw_input("\033[1;96m[#] \x1b[0;36m Enter Username\x1b[1;92m➤ ")
-    if (username == CorrectUsername):
-    	password = raw_input("\033[1;96m[#] \x1b[0;36m Enter Password\x1b[1;92m➤ ")
-        if (password == CorrectPassword):
-            print "Logged in successfully as " + username #fb-cloning-id SG
-            loop = 'false'
-        else:
-            print "Wrong password!"
-            os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
-    else:
-        print "Wrong username!"
-        os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
-
 import requests,random,json,time,sys,os,re
 # -----------------------------------------------------------
 # author ShuBham
@@ -311,4 +292,4 @@ def jnspam():
 		elif( oy == '0' or oy == '00' ):
 			sys.exit()
 		else:
-			print(m+'        
+			print(m+'       
